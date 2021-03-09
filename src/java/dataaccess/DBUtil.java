6db512075f5953fb1bfd7e16e2588da8ebf2cead
815822 <815822@SAIT225788IT.ACDM.DS.SAIT.CA>
2021-03-09 14:07:44 -0700

    Updated UserService.java

--- src/java/dataaccess/DBUtil.java
+++ src/java/dataaccess/DBUtil.java
@@ -5,10 +5,26 @@
  */
 package dataaccess;
 
-/**
- *
- * @author 841898
- */
+import java.sql.*;
+
 public class DBUtil {
-    
+    public static void closePreparedStatement(Statement ps) {
+        try {
+            if (ps != null) {
+                ps.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
 }
