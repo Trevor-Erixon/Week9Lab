177aaf7b89094a22353eaf979178015ae29db8c4
Trevor-Erixon <Trevor-Erixon>
2021-03-16 13:34:58 -0600

    Removed Role and User beans, ConnectionPool class, and updated DBUtil

--- src/java/dataaccess/DBUtil.java
+++ src/java/dataaccess/DBUtil.java
@@ -1,30 +1,14 @@
 
 package dataaccess;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
-import java.sql.*;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class DBUtil {
-    public static void closePreparedStatement(Statement ps) {
-        try {
-            if (ps != null) {
-                ps.close();
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-    }
-
-    public static void closeResultSet(ResultSet rs) {
-        try {
-            if (rs != null) {
-                rs.close();
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
+    public static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("UserPU");
+    
+    public static EntityManagerFactory getEmFactory() {
+        return EMF;
     }
 }
